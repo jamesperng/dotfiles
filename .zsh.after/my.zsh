b7336e9fd2b718b348323a2a21f05c236f441ec9
James Perng <james.perng95@gmail.com>
2017-11-19 18:14:31 -0800

    remove exports from school

--- .zsh.after/my.zsh
+++ .zsh.after/my.zsh
@@ -1,6 +1,2 @@
 prompt agnoster
 alias server_jp='ssh root@104.236.152.136'
-export MACHTYPE=x86_64mac
-export PROG_ROOT=/Users/jamesperng/school/545/cs545-lab/prog
-export LAB_ROOT=/Users/jamesperng/school/545/cs545-lab/prog_root
-export PATH=./$MACHTYPE:$PATH
